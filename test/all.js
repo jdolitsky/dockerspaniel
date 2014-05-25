a49e92981976bdcf867cc156c1007ea99c24ae6c
Josh Dolitsky <jdolitsky@gmail.com>
2014-05-24 19:40:48 -0700

    making it require-able, adding tests/travisci

---
+++ test/all.js
@@ -0,0 +1,15 @@
+var async = require('async');
+var should = require('should');
+
+
+var dockerspaniel_path = '../lib/dockerspaniel.js';
+
+describe('dockerspaniel', function() {
+    describe('module', function() {
+        it('can be required', function() {
+            (function() {
+                require(dockerspaniel_path);
+            }).should.not.throw ();
+        });
+    });
+});
