a4470207f54bdbb79ffc733121e63370dc953d40
Josh Dolitsky <jdolitsky@gmail.com>
2014-05-24 20:31:43 -0700

    adding more tests

--- test/all.js
+++ test/all.js
@@ -3,11 +3,66 @@ var should = require('should');
 var dockerspaniel_path = '../lib/dockerspaniel.js';
 
 describe('dockerspaniel', function() {
+    
+    var ds;
+
     describe('module', function() {
         it('can be required', function() {
             (function() {
-                require(dockerspaniel_path);
+                ds = require(dockerspaniel_path);
             }).should.not.throw ();
-        });
-    });
-});
+        })
+    })
+
+    describe('generateContents() method', function() {
+        it('exists', function() {
+            ds.should.have.property('generateContents');
+        })
+        
+        var spaniel = {
+            from: 'ubuntu:12.04',
+            maintainer: 'Joe Somebody',
+            steps: [
+                {
+                    instruction: 'run',
+                    arguments: 'apt-get update',
+                    unless: ['noupdate']
+                },
+                {
+                    instruction: 'run',
+                    arguments: 'apt-get install -y nodejs',
+                    only: ['nodejs']
+                }
+            ]
+        };
+
+        var tags;
+
+        it('generates contents correctly', function(done) {
+            tags = [];
+            ds.generateContents(spaniel, tags, function(err, contents) {
+                should.not.exist(err);
+                should.exists(contents);
+                contents.should.equal('FROM ubuntu:12.04\nMAINTAINER Joe Somebody\nRUN apt-get update');
+                done();
+            });
+        })
+        
+        it('generates contents correctly with tags', function(done) {
+            tags = ['noupdate', 'nodejs'];
+            ds.generateContents(spaniel, tags, function(err, contents) {
+                should.not.exist(err);
+                should.exists(contents);
+                contents.should.equal('FROM ubuntu:12.04\nMAINTAINER Joe Somebody\nRUN apt-get install -y nodejs');
+                done();
+            });
+        })
+    })
+    
+    describe('createDockerfile() method', function() {
+        it('exists', function() {
+            ds.should.have.property('createDockerfile');
+        })
+    })
+
+})
