cc3e4daf427f877943ba986ef10eec011f059ff0
Josh Dolitsky <jdolitsky@gmail.com>
2014-05-25 03:01:35 -0700

    adding --help option

--- bin/dockerspaniel-cli.js
+++ bin/dockerspaniel-cli.js
@@ -1,25 +1,34 @@
 #!/usr/bin/env node
 
+var yargs = require('yargs');
 var ds = require('../lib/dockerspaniel.js');
 var defaults = require('../defaults.json');
 
 
 // parse command line options
-var argv = require('yargs')
+var argv = yargs
     .usage(
-        'dockerspaniel -i [input] -o [output] -b [base image] -t [tag]\n'+
-        '-i | --input    JSON file to use for input (default: Spanielfile)\n'+
-        '-o | --output   Path of new Dockerfile to create (default: Dockerfile)\n'+
-        '-b | --base     Override Docker base image to use in FROM instruction\n'+
-        '-t | --tag      Tag(s) to prevent or include items based on \'unless\' and \'only\'')
+        '\n  Usage: dockerspaniel -i [input] -o [output] -b [baseimage] -t [tag]\n\n'+
+        '  -h|--help    This help screen\n'+
+        '  -i|--input   JSON file to use for input (default: Spanielfile)\n'+
+        '  -o|--output  Path of new Dockerfile to create (default: Dockerfile)\n'+
+        '  -b|--base    Override Docker base image to use in FROM instruction\n'+
+        '  -t|--tag     Tag(s) for use by \'unless\' and \'only\' step attributes\n'+
+        '               Supports mutliple tags via format \'-t tag1 -t tag2\' etc.')
     .alias('t','tag')
     .alias('b','base')
     .alias('i','input')
     .alias('o','output')
+    .alias('h','help')
     .argv;
 
 
 var main = function () {
+
+    if (argv.h) {
+        yargs.showHelp();
+        process.exit(0);
+    }
     
     var options = {
         input: argv.i,
